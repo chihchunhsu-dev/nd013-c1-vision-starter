b74f8c37f6b31db84744eca101a1c8c2fecb16ce
chihchunhsu-dev <chihchunhsu.me@gmail.com>
2022-01-10 20:17:37 +0800

    Create train/val/test dataset splits

--- create_splits.py
+++ create_splits.py
@@ -3,6 +3,9 @@ import glob
 import os
 import random
 
+import shutil
+from sklearn.model_selection import train_test_split
+
 import numpy as np
 
 from utils import get_module_logger
@@ -17,8 +20,20 @@ def split(source, destination):
         - source [str]: source data directory, contains the processed tf records
         - destination [str]: destination data directory, contains 3 sub folders: train / val / test
     """
-    # TODO: Implement function
+    tfrecords = glob.glob(os.path.join(source, "*.tfrecord"))
+    train_files, val_test_files = train_test_split(tfrecords, test_size=0.4)
+    val_files, test_files= train_test_split(val_test_files, test_size=0.5)
+    
+    train_dir = os.path.join(destination, 'train')
+    val_dir = os.path.join(destination, 'val')
+    test_dir = os.path.join(destination, 'test')
 
+    files_list = [train_files, val_files, test_files]
+    dirs = [train_dir, val_dir, test_dir]
+
+    for files, dir in zip(files_list, dirs):
+        for file in files:
+            shutil.move(file, dir)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Split data into training / validation / testing')
